7b5b9380444331b64f8171d9e1fce8f0ebcfb732
Ignati Voronov <gntvrnv@gmail.com>
2023-04-21 16:07:12 +0300

    Прошло секунд с начала суток

--- src/Main.java
+++ src/Main.java
@@ -2,46 +2,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String number;
+        int hours;
+        int minutes;
+        int seconds;
 
-        for(;;) {
-            Scanner in = new Scanner(System.in);
-            System.out.print("Please enter the number from 100 to 2147483647 or STOP ");
-            number = in.next();
+        Scanner in = new Scanner(System.in);
+        System.out.print("Hours ");
+        hours = in.nextInt();
 
-            //System.out.println(number.equals("STOP"));
+        System.out.print("Minutes ");
+        minutes = in.nextInt();
 
-            if(number.equals("STOP")){
-                System.out.println("Bye!");
-                break;
-            }else if(errorFinder(number)) {
-                System.out.println("You have entered an invalid character - please try again.");
-            }else thirdDigit(number);
-        }
+        System.out.print("Seconds ");
+        seconds = in.nextInt();
 
+        secPass(hours, minutes, seconds);
 
     }
+    public static void secPass(int hours, int minutes, int seconds){
+        int sum;
 
-    //проверка наличия иных символов кроме цифр
-    public static boolean errorFinder(String integ){
-        boolean errorFlag = false;
+        sum = seconds+minutes*60+hours*3600;
 
-        for (int i=0;i<integ.length();i++){
-            if(((int)integ.charAt(i))<48 || ((int)integ.charAt(i))>57){
-                errorFlag = true;
-                break;
-            }else errorFlag=false;
-        }
-        return errorFlag;
+        System.out.println(sum+" seconds have passed");
     }
 
-    //поиск нужной цифры
-    public static void thirdDigit(String integ){
-
-        char digit;
-
-        digit = integ.charAt(integ.length()-3);
-
-        System.out.println("Third digit from the right - "+digit);
-    }
 }
