10099ea1ec9aeb5bf532af211c7585417a76dccf
Ignati Voronov <gntvrnv@gmail.com>
2023-04-29 16:41:53 +0300

    Прошло секунд с начала суток (v2)

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        /**
+         * Тривиальная (3 балла).
+         *
+         * Задача имеет повышенную стоимость как первая в списке.
+         *
+         * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
+         * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
+         */
+
+       getSec();
+
+    }
+
+    //подсчет секунд
+    public static void secPassCalk(int hours, int minutes, int seconds){
+        int sum;
+
+        sum = seconds+minutes*60+hours*3600;
+
+        System.out.println(sum+" seconds have passed");
+    }
+
+    public static void getSec(){
         int hours;
         int minutes;
         int seconds;
@@ -16,15 +40,7 @@ public class Main {
         System.out.print("Seconds ");
         seconds = in.nextInt();
 
-        secPass(hours, minutes, seconds);
-
-    }
-    public static void secPass(int hours, int minutes, int seconds){
-        int sum;
-
-        sum = seconds+minutes*60+hours*3600;
-
-        System.out.println(sum+" seconds have passed");
+        secPassCalk(hours, minutes, seconds);
     }
 
 }
