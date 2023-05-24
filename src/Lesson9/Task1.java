ced0d96e88192a7967c7867b26101b4eabdf12e4
Ignati Voronov <gntvrnv@gmail.com>
2023-05-24 12:58:35 +0300

    Лекция 9 Центрировать заданный список list

---
+++ src/Lesson9/Task1.java
@@ -0,0 +1,61 @@
+package Lesson9;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Task1 {
+    public static void main(String[] args) {
+        /**
+         * Средняя (3 балла)
+         *
+         * Центрировать заданный список list, уменьшив каждый элемент на среднее арифметическое всех элементов.
+         * Если список пуст, не делать ничего. Вернуть изменённый список.
+         *
+         * Обратите внимание, что данная функция должна изменять содержание списка list, а не его копии.
+         */
+
+        System.out.println(centeredList(listBuilder()));
+    }
+
+
+    private static ArrayList<Double> listBuilder() {
+        ArrayList<Double> listOfNum = new ArrayList<>();
+        String[] numbs;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите числа через пробел");
+        String input = in.nextLine();
+
+
+        if (input.isEmpty()) {
+            return null;
+        } else {
+            numbs = input.split(" ");
+            for (String s : numbs) {
+                listOfNum.add(Double.valueOf(s));
+            }
+        }
+
+        return listOfNum;
+    }
+
+    private static ArrayList<Double> centeredList(ArrayList<Double> arrayList) {
+        double averageValue = 0;
+
+        if (arrayList == null) {
+            return null;
+        } else {
+            for (double d : arrayList) {
+                averageValue += d;
+            }
+            averageValue /= arrayList.size();
+            for (int i = 0; i < arrayList.size(); i++) {
+                arrayList.set(i, arrayList.get(i) - averageValue);
+            }
+            return arrayList;
+
+        }
+    }
+}
